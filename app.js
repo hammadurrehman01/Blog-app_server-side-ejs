a5e7769dfb2a368c3a82859d37c8185b3df63e64
hammadurrehman01 <hammadurrehman1954@gmail.com>
2025-03-27 12:56:48 +0500

    refactor app.js for improved readability; update middleware usage and enhance home route; add new image uploads; update navbar to display user's full name

--- app.js
+++ app.js
@@ -12,34 +12,30 @@ import dotenv from "dotenv";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.set("view engine", "ejs");
-app.use(checkAuthCookie('token'))
+app.use(checkAuthCookie("token"));
 app.set("views", path.resolve("./views"));
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.resolve("./public")))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve("./public")));
 
-app.use('/user', userRoute)
-app.use('/blog', blogRoute)
-app.use('/comment', commentRoute)
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+app.use("/comment", commentRoute);
 
-dotenv.config()
+dotenv.config();
 connectDB();
 
-app.get('/', async (req, res) => {
-    const allBlogs = await Blog.find().sort({ createdAt: -1 })
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find().sort({ createdAt: -1 });
 
-    return res.render("home", {
-        user: req.user,
-        blogs: allBlogs
-    })
-})
-
-console.log(process.env);
-
+  return res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
-
+  console.log(`Server is listening on port ${PORT}`);
+});
